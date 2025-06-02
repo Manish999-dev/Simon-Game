78df5e53bb6dcbe36fcac9af6463551fba480952
manish2056 <k22rs085@st.kyusan-u.ac.jp>
2025-06-02 15:52:52 +0900

    Just added a new function called animatePress which makes the pressed button light white around its border

--- game.js
+++ game.js
@@ -1,46 +1,43 @@
-// Array of color options
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-// To store the game's pattern sequence
 var gamePattern = [];
-
 var userClickedPattern = [];
 
-//1. Using jQuery to detect when any of the buttons are clicked and trigger a handler function.
 $(".btn").click(function() {
-   
-    //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
- 
-    var userChosenColour = $(this).attr("id");
 
-    //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
-//   console.log(userClickedPattern);
-
-    //1. In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
   playSound(userChosenColour);
+
+  animatePress(userChosenColour);
 });
 
-
 function nextSequence() {
-    // Random number from 0 to 3
-    var randomNumber = Math.floor(Math.random() * 4);
 
-    // Use it to pick a random color
-    var randomChosenColour = buttonColours[randomNumber];
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-    // Add color to game pattern
-    gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    // Animate the chosen button
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    // Refactoring the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
-    playSound(randomChosenColour);
+  playSound(randomChosenColour);
 }
 
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
+function animatePress(currentColor) {
+
+  //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+  $("#" + currentColor).addClass("pressed");
+
+  //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
