3d978ca772f1c2dce68bd71d8d1f0c00d96ba7b9
manish2056 <k22rs085@st.kyusan-u.ac.jp>
2025-06-02 16:11:09 +0900

    Implement game start on first keypress, initialize level tracking, and update h1 title with current level in nextSequence()

--- game.js
+++ game.js
@@ -4,6 +4,24 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Creating a new variable called level and start at level 0.
+var level = 0;
+
+//1. Using jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+  if (!started) {
+
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
@@ -15,6 +33,12 @@ $(".btn").click(function() {
 });
 
 function nextSequence() {
+
+  //4. Inside nextSequence(), increasing the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), updating the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
